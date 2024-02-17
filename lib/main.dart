e0b76c136a99afa5caa08daa740f7692e00b88a0
iropazu <c1342318@st.kanazawa-it.ac.jp>
2024-02-17 13:56:48 +0900

    円グラフの追加とfirebaseに対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:studytime/next_page.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
+    const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '勉強記録アプリ',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 6, 159, 19)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: '勉強時間記録アプリ'),
+      home: piechartSample(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
+class piechartSample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<PieChartSectionData> sections = List.generate(6, (index) {
+      final double radius = 50;
+      switch (index) {
+        case 0:
+          return PieChartSectionData(
+            color: Colors.blue,
+            value: 40,
+            title: 'sample1',
+            radius: radius,
+            titleStyle: TextStyle(
+              fontSize: 8,fontWeight: FontWeight.bold,color: Colors.white),
+          );
+        case 1:
+          return PieChartSectionData(
+            color: Colors.red,
+            value: 20,
+            title: 'sample2',
+            radius: radius,
+            titleStyle: TextStyle(
+              fontSize: 8,fontWeight: FontWeight.bold,color: Colors.white),
+          );
+        case 2:
+         return PieChartSectionData(
+          color: Colors.green,
+          value: 15,
+          title: 'sample3',
+          radius: radius,
+          titleStyle: TextStyle(
+            fontSize: 8,fontWeight: FontWeight.bold,color: Colors.white),
+         );
+        case 3:
+         return PieChartSectionData(
+          color: Colors.orange,
+          value: 10,
+          title: 'sample4',
+          radius: radius,
+          titleStyle: TextStyle(
+            fontSize: 8,fontWeight: FontWeight.bold,color: Colors.white),
+         );
+        case 4:
+         return PieChartSectionData(
+          color: Colors.purple,
+          value: 10,
+          title: 'sample5',
+          radius: radius,
+          titleStyle: TextStyle(
+            fontSize: 8,fontWeight: FontWeight.bold,color: Colors.white),
+         );
+        case 5:
+         return PieChartSectionData(
+          color: Colors.yellow,
+          value: 5,
+          title: 'sample6',
+          radius: radius,
+          titleStyle: TextStyle(
+            fontSize: 8,fontWeight: FontWeight.bold,color: Colors.white)
+         );
+        default:
+         return null!;
+      }
+    });
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('勉強時間記録アプリ'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left:100.0),
+      body: Center(
         child: Column(
           children: [
             SizedBox(
@@ -57,8 +110,19 @@ class _MyHomePageState extends State<MyHomePage> {
               },
               )
             ),
-            Padding(padding: const EdgeInsets.only(top:200.0)),
-            const Text('円グラフ')
+            const Text('円グラフ'),
+            SizedBox(
+              width:200,
+              height:150,
+              
+              child:PieChart(
+                PieChartData(
+                  sectionsSpace: 0,
+                  centerSpaceRadius:20,
+                  sections: sections,
+                ),
+              ),
+            ),
           ]
         ),
       ),
